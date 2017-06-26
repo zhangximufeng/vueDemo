08a1816bf587e2ad69444b34038a50b5bb487b70
zhangximufeng <357321515@qq.com>
2017-06-26 16:21:56 +0800

    前端项目

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+	<div>
+		<transition name="router-fade" mode="out-in">
+			<keep-alive>
+			    <router-view v-if="$route.meta.keepAlive"></router-view>
+			</keep-alive>
+    	</transition>
+    	<transition name="router-fade" mode="out-in">
+			<router-view v-if="!$route.meta.keepAlive"></router-view>
+		</transition>
+    </div>
+</template>
+
+<script>
+
+  	export default {
+    	
+  	}
+
+</script>
+
+<style lang="scss">
+  	@import './style/common';
+	.router-fade-enter-active, .router-fade-leave-active {
+	  	transition: opacity .3s;
+	}
+	.router-fade-enter, .router-fade-leave-active {
+	  	opacity: 0;
+	}
+</style>
